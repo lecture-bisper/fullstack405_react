a8e913d528df7435bd3a89119d439d2c09e709f0
lecture-bisper <bisper@outlook.kr>
2024-08-16 16:53:49 +0900

    리액트 3일차 - 1.자바스크립트의 비동기 방식 실행    2. 프로미스를 사용한 비동기 방식 실행    3. 모듈 사용하기    4. 배열 함수 forEach, map    5. 리액트 프로젝트 생성하기    6. 리액트 컴포넌트 사용하기

---
+++ promiseTest/src/main/java/bitc/fullstack405/promisetest/PromiseTestApplication.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.promisetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromiseTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PromiseTestApplication.class, args);
+  }
+
+}
---
+++ promiseTest/src/test/java/bitc/fullstack405/promisetest/PromiseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.promisetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromiseTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
